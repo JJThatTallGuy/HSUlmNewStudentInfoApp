6648bb2017e755c6e376006a16d2f6fbaed54ac3
Abbas <abbasbushehri2@gmail.com>
2018-11-19 19:07:40 +0100

    Merged with new master

---
+++ app/src/main/gen/edu/rosehulman/jonesjg1/hs_ulmnewstudentinfoapp/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package edu.rosehulman.jonesjg1.hs_ulmnewstudentinfoapp;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
---
+++ app/src/main/gen/edu/rosehulman/jonesjg1/hs_ulmnewstudentinfoapp/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.rosehulman.jonesjg1.hs_ulmnewstudentinfoapp;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
