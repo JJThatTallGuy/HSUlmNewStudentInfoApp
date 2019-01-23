a53d956abd8011776f59116939fc4d224303522f
JJThatTallGuy <jjones2406@gmail.com>
2019-01-23 13:35:17 +0100

    scroll to bottom added

--- app/src/main/java/edu/rosehulman/jonesjg1/hs_ulmnewstudentinfoapp/MainActivity.java
+++ app/src/main/java/edu/rosehulman/jonesjg1/hs_ulmnewstudentinfoapp/MainActivity.java
@@ -229,6 +229,10 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    public GoogleSignInAccount getaccount(){
+        return this.maccount;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
